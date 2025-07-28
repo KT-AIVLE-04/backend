a56a0d8e9a4c2eb1593fba6bdf78b299d372a628
charmeee <mandugohigh@gmail.com>
2025-07-29 03:26:32 +0900

    fix: AuthExcludePaths 인증 제외 경로 추가 및 재활용을 위한 common폴더로 이동
    - auth에서 permisson path가 필요할 상황이 있어 이를 재활용하면 좋겟다고 판단 -> common으로 이동

--- common/src/main/java/kt/aivle/common/config/AuthExcludePaths.java
+++ common/src/main/java/kt/aivle/common/config/AuthExcludePaths.java
@@ -1,14 +1,14 @@
-package kt.aivle.gateway.config;
+package kt.aivle.common.config;
 
 import java.util.List;
 
 public class AuthExcludePaths {
     public static final List<String> EXCLUDE_PATHS = List.of(
         "/api/auth/login",
-        "/api/auth/signup",
+        "/api/auth/signup", 
         "/api/auth/refresh",
         "/api/auth/logout",
-        "/api/auth/oauth2/google",
-        "/api/auth/oauth2/callback"
+        "/oauth2/authorization/**",
+        "/login/oauth2/code/**"
     );
-}
+} 
