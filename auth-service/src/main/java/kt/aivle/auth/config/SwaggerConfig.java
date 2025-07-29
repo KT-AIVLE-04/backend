c4c5a9e977a461d441891448f94d3c3e04fa1286
leejongsuu <lik9819@naver.com>
2025-07-29 10:24:19 +0900

    feat: swagger 문서 적용
    - Gateway에서 Proxy 방식으로 Swagger 문서 기능 구현
    - Gateway에 Swagger UI만 띄우고, 각 서비스의 `/v3/api-docs`를 프록시로 불러오기

---
+++ auth-service/src/main/java/kt/aivle/auth/config/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package kt.aivle.auth.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+@OpenAPIDefinition
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT");
+
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
+
+        return new OpenAPI()
+                .addServersItem(new Server().url("/"))
+                .info(new Info().title("Auth Service API").version("v0.0.1"))
+                .components(
+                        new io.swagger.v3.oas.models.Components()
+                                .addSecuritySchemes("bearerAuth", securityScheme)
+                )
+                .security(List.of(securityRequirement));
+    }
+}
