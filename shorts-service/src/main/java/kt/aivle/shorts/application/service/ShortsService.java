564c60dcf8cf34c7b09b9118853ad2bd91af8404
leejongsuu <lik9819@naver.com>
2025-08-07 16:28:49 +0900

    [shorts] refactor: 시나리오 생성 로직 수정
    - ShortsService에서 롤백/정합성을 위해 에러시 S3 이미지 삭제 추가
    - Event 발행할 때 이미지 정보가 필요하기 때문에 기존 url만 반환하던걸 메타정보까지 포함해서 반환하도록 함
    - Event 발행 실패 에러코드 추가

--- shorts-service/src/main/java/kt/aivle/shorts/application/service/ShortsService.java
+++ shorts-service/src/main/java/kt/aivle/shorts/application/service/ShortsService.java
@@ -2,9 +2,12 @@ package kt.aivle.shorts.application.service;
 
 import kt.aivle.common.exception.BusinessException;
 import kt.aivle.shorts.adapter.in.web.dto.ScenarioResponse;
+import kt.aivle.shorts.adapter.out.event.CreateContentRequestEvent;
+import kt.aivle.shorts.adapter.out.s3.UploadedImageInfo;
 import kt.aivle.shorts.application.port.in.ShortsUseCase;
 import kt.aivle.shorts.application.port.in.command.CreateScenarioCommand;
 import kt.aivle.shorts.application.port.out.AiWebClientPort;
+import kt.aivle.shorts.application.port.out.ContentsEventPort;
 import kt.aivle.shorts.application.port.out.ImageStoragePort;
 import kt.aivle.shorts.application.port.out.StoreInfoQueryPort;
 import kt.aivle.shorts.application.service.dto.ScenarioDto;
@@ -12,6 +15,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 import static kt.aivle.shorts.exception.ShortsErrorCode.*;
 
@@ -23,6 +28,7 @@ public class ShortsService implements ShortsUseCase {
     private final StoreInfoQueryPort storeInfoQueryPort;
     private final AiWebClientPort aiWebClientPort;
     private final ImageStoragePort imageStoragePort;
+    private final ContentsEventPort contentsEventPort;
 
     @Override
     public Mono<ScenarioResponse> createScenario(CreateScenarioCommand command) {
@@ -34,10 +40,22 @@ public class ShortsService implements ShortsUseCase {
                                         imageStoragePort.uploadImages(imageList)
                                                 .onErrorMap(e -> new BusinessException(IMAGE_UPLOAD_ERROR, e.getMessage()))
                                 )
-                                .flatMap(urlList -> {
+                                .flatMap(uploadedImageInfos -> {
+                                    List<String> urlList = uploadedImageInfos.stream()
+                                            .map(UploadedImageInfo::url)
+                                            .toList();
+
                                     ScenarioDto scenarioDto = ScenarioDto.from(command, storeInfo, urlList);
+
                                     return aiWebClientPort.callScenario(scenarioDto)
-                                            .onErrorMap(e -> new BusinessException(AI_WEB_CLIENT_ERROR, e.getMessage()));
+                                            .onErrorResume(aiError -> imageStoragePort.deleteImages(uploadedImageInfos)
+                                                    .then(Mono.error(new BusinessException(AI_WEB_CLIENT_ERROR, aiError.getMessage()))))
+                                            .flatMap(scenarioResponse -> contentsEventPort.publish(new CreateContentRequestEvent(command.storeId(), uploadedImageInfos)
+                                                    )
+                                                    .thenReturn(scenarioResponse)
+                                                    .onErrorResume(eventError -> imageStoragePort.deleteImages(uploadedImageInfos)
+                                                            .then(Mono.error(new BusinessException(CONTENTS_EVENT_ERROR, eventError.getMessage())))));
+
                                 })
                 );
     }
