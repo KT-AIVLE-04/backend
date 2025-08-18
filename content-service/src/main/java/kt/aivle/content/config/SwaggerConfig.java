b6aa2c898c4303704d59fce98c2d90a3fecd2b2d
leejongsuu <lik9819@naver.com>
2025-08-18 11:26:18 +0900

    [content] feat: swagger
    - 스웨거 설정 추가

---
+++ content-service/src/main/java/kt/aivle/content/config/SwaggerConfig.java
@@ -0,0 +1,51 @@
+package kt.aivle.content.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.parameters.Parameter;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springdoc.core.customizers.OpenApiCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+@OpenAPIDefinition
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT");
+
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
+
+        return new OpenAPI()
+                .addServersItem(new Server().url("/"))
+                .info(new Info().title("Content Service API").version("v0.0.1"))
+                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+                .security(List.of(securityRequirement));
+    }
+
+    @Bean
+    public OpenApiCustomizer removeUserIdHeader() {
+        return openApi -> {
+            openApi.getPaths().values()
+                    .forEach(pathItem ->
+                            pathItem.readOperations()
+                                    .forEach(op -> {
+                                        List<Parameter> params = op.getParameters();
+                                        if (params != null) {
+                                            params.removeIf(p -> "X-USER-ID".equals(p.getName()));
+                                        }
+                                    })
+                    );
+        };
+    }
+}
