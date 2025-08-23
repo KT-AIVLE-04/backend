4ff04ec1817d9fe7ebb8a38719f324eec8535d20
charmeee <mandugohigh@gmail.com>
2025-08-24 03:40:35 +0900

    feat: timeout설정추가

--- analytics-service/src/main/java/kt/aivle/analytics/config/RestTemplateConfig.java
+++ analytics-service/src/main/java/kt/aivle/analytics/config/RestTemplateConfig.java
@@ -2,6 +2,7 @@ package kt.aivle.analytics.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -9,6 +10,12 @@ public class RestTemplateConfig {
     
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+
+        // 타임아웃 설정 
+        factory.setConnectTimeout(30000);  // 연결 타임아웃: 30초
+        factory.setReadTimeout(300000);    // 읽기 타임아웃: 5분 (300초)
+        
+        return new RestTemplate(factory);
     }
 }
