c4ef8d5038636544d6321a3ffb1589b5130c5c24
charmeee <mandugohigh@gmail.com>
2025-08-24 02:27:46 +0900

    feat: jpa repo 명시적으로 지정

--- analytics-service/src/main/java/kt/aivle/analytics/AnalyticsServiceApplication.java
+++ analytics-service/src/main/java/kt/aivle/analytics/AnalyticsServiceApplication.java
@@ -2,11 +2,13 @@ package kt.aivle.analytics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = {"kt.aivle.analytics", "kt.aivle.common"})
+@EnableJpaRepositories(basePackages = "kt.aivle.analytics.adapter.out.persistence.repository")
 @EnableScheduling
 @EnableRetry
 @EnableAsync
